6b4b4ecc5e5f559ae9637194312260c2168a23a8
Alexandr Ivanov <stuardryan14529@gmail.com>
2025-03-27 22:48:35 +0300

    Реализованы функции для считывания файла excel и поиска по различным параметрам

---
+++ src/services.py
@@ -0,0 +1,73 @@
+import json
+import logging
+import re
+
+import pandas as pd
+
+from config import ROOT_DIR
+
+logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler(
+    ROOT_DIR + "/logs/services.log", mode="w", encoding="utf-8"
+)
+file_formatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
+
+
+def read_excel_file(file_path: str = ROOT_DIR + "/data/operations.xlsx") -> list[dict]:
+    """Функция для чтения excel файла"""
+    try:
+        df = pd.read_excel(file_path)
+        result = df.to_dict("records")
+        logger.info(f"Файл в директории {file_path} найден и обработан")
+        return result
+    except FileNotFoundError as e:
+        logger.error("Файл не найден")
+        raise e
+    except Exception as e:
+        logger.error(f"Произошла непредвиденная ошибка: {e}")
+        raise e
+
+
+def rec_search_request(search_word: str | None) -> str:
+    """Функция получает запрос поиска от пользователя по строке"""
+    if search_word:
+        excel_data = read_excel_file()
+        get_string = [
+            data
+            for data in excel_data
+            if search_word in str(data["Категория"])
+            or search_word in str(data["Описание"])
+        ]
+        logger.info("Данные получены")
+        return json.dumps(get_string)
+    logger.error("Значение для поиска ничего не содержит")
+    raise ValueError("Значение для поиска ничего не содержит")
+
+
+def search_mobile_number_excel() -> str:
+    """Функция для поиска мобильных номеров в поле 'Категория'"""
+    data_mobile_number = []
+    excel_data = read_excel_file()
+    for data in excel_data:
+        re_search = re.search(r"\+7 \d{3} \d{2,3}-\d{2}-\d{2}", str(data["Категория"]))
+        if re_search:
+            data_mobile_number.append(data)
+    logger.info("Данные считаны")
+    return json.dumps(data_mobile_number)
+
+
+def search_transactions_to_people() -> str:
+    """Функция для поиска переводов физ лицам"""
+    sent_transaction = []
+    excel_data = read_excel_file()
+    for data in excel_data:
+        re_search = re.search(
+            r"^[А-ЯЁ][а-яё]+\s+[А-ЯЁ]\.$", str(data["Категория"]), re.I
+        )
+        if "Переводы" in str(data["Категория"]) and re_search:
+            sent_transaction.append(data)
+    logger.info("Данные по переводам считаны")
+    return json.dumps(sent_transaction)
